d9475244b83c33abb79af7afb8a560c440f213f7
Ruby Morgan Voigt <87840081+1612elphi@users.noreply.github.com>
2025-11-08 02:29:12 +0100

    Revert "Fix smile icon visibility in navbar"

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -73,8 +73,6 @@ export function Navbar() {
             width={40}
             height={40}
             className="h-10 w-10"
-            unoptimized
-            priority
           />
         </Link>
 
